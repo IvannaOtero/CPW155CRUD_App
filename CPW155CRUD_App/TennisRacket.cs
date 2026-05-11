456700547ec6dd63486609cb8722ac19a243d75c
IvannaOtero <iotero1982@students.cptc.edu>
2026-05-11 14:49:25 -0700

    Add TennisRacket class with properties and documentation

---
+++ CPW155CRUD_App/TennisRacket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPW155CRUD_App;
+
+
+/// <summary>
+/// Represents a single physical tennis racket to sell. 
+/// </summary>
+public class TennisRacket
+{
+    public int ItemId {  get; set; }
+
+    /// <summary>
+    /// the weights in ounces of the tennis racket
+    /// </summary>
+    public byte Weight { get; set; }
+
+
+    /// <summary>
+    /// The size of the racket head in centemeters 
+    /// </summary>
+    public short HeadSize { get; set; } 
+
+
+    /// <summary>
+    ///  the brand name or manufacturer of the tennis racket
+    /// </summary>
+    public required string Brand { get; set; }
+
+    /// <summary>
+    /// the price the racket will sell at retail
+    /// </summary>
+    public double Price { get; set; }
+}
